03f3b26b57341d88833f3747f18cb7c07d5a6e06
Edwards-Github <84117648+Edwards-Github@users.noreply.github.com>
2026-06-19 19:17:50 -0700

    - finished thank you state from the javascript I learned

--- rating.js
+++ rating.js
@@ -1,6 +1,7 @@
-let image = document.querySelector('.star-icon');
-let ratings = document.querySelectorAll('.button');
-let submitButton = document.querySelector('.submit-button');
+const image = document.querySelector('.star-icon');
+const ratings = document.querySelectorAll('.button');
+const submitButton = document.querySelector('.submit-button');
+let button = document.querySelectorAll('.button');
 
 let userRating = '';
 ratings.forEach(rating => {
@@ -9,12 +10,22 @@ ratings.forEach(rating => {
   });
 });
 
+button.forEach(btn => {
+  btn.addEventListener('click', function (event) {
+    btn.style.color = 'black';
+    btn.style.backgroundColor = 'hsl(0, 100%, 100%)';
+  })
+})
+
 submitButton.addEventListener('click', function (event) {
   image.setAttribute('src', 'assets/illustration-thank-you.svg');
-  changeHeader();
-  changeDescription();
-  removeRating();
-  displayUserRating();
+  image.style.backgroundColor = 'hsl(216, 12%, 8%)';
+  changeHeader( );
+  changeDescription( );
+  removeRating( );
+  removeSubmitButton( );
+  displayUserRating( );
+  adjustContainer( );
 });
 
 function changeHeader ( ){
@@ -24,6 +35,7 @@ function changeHeader ( ){
 
 function changeDescription ( ){
   let description = document.querySelector('.survey-description');
+  description.style.textAlign = 'center';
   description.innerText = "We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!";
 }
 
@@ -32,8 +44,24 @@ function removeRating ( ){
   buttons.remove( );
 }
 
-function displayUserRating() {
+function removeSubmitButton ( ){
+  let submitButton = document.querySelector('.submit-button');
+  submitButton.remove( );
+}
+
+function displayUserRating( ) {
   let userRatingClass = document.querySelector('.user');
   userRatingClass.classList.add('user-rating')
   userRatingClass.innerText = `You selected ${userRating} out of 5`;
 }
+
+function adjustContainer() {
+  let container = document.querySelector('.container');
+  container.style.height = '450px';
+  container.style.display = 'flex';
+  container.style.flexDirection = 'column';
+  container.style.justifyContent = 'center';
+  container.style.alignItems = 'center';
+  container.style.padding = '30px';
+}
+
